d8b5830cbab2865a3cbd9f18618094b67c3c3a2a
Kaio Antônio <kaioaar@gmail.com>
2022-08-19 10:15:59 -0300

    add: all controllers and repositories

---
+++ src/main/java/com/pdv/repository/ItemSaleRepository.java
@@ -0,0 +1,10 @@
+package com.pdv.repository;
+
+import com.pdv.entity.ItemSale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemSaleRepository extends JpaRepository<ItemSale, Long> {
+
+}
---
+++ src/main/java/com/pdv/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.pdv.repository;
+
+import com.pdv.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
