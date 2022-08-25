6c0c3fe13232929740f7efd75a8f13b2c5e64752
Kaio Antônio <kaioaar@gmail.com>
2022-08-25 11:05:29 -0300

    feat: implementing dto for user entity

--- src/main/java/com/pdv/pdv/controller/UserController.java
+++ src/main/java/com/pdv/pdv/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.pdv.pdv.controller;
 
+import com.pdv.pdv.dto.ResponseDTO;
+import com.pdv.pdv.dto.UserDTO;
 import com.pdv.pdv.entity.User;
+import com.pdv.pdv.exceptions.NoItemException;
 import com.pdv.pdv.repository.UserRepository;
+import com.pdv.pdv.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +19,22 @@ import java.util.Optional;
 @RequestMapping("/user")
 public class UserController {
 
-    private UserRepository userRepository;
+    private UserService userService;
 
-    public UserController(@Autowired UserRepository userRepository){
-        this.userRepository = userRepository;
+    public UserController(@Autowired UserService userService){
+        this.userService = userService;
     }
 
     @GetMapping()
     public ResponseEntity getAll(){
-        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
     @PostMapping()
     public ResponseEntity post(@RequestBody User user){
         try{
             user.setEnabled(true);
-            return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+            return new ResponseEntity<>(userService.save(user), HttpStatus.OK);
         }catch (Exception error){
             return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -38,19 +42,20 @@ public class UserController {
 
     @PutMapping()
     public ResponseEntity put(@RequestBody User user){
-        Optional<User> userToEdit = userRepository.findById(user.getId());
-        if(userToEdit.isPresent()){
-            userRepository.save(user);
-            return new ResponseEntity<>(user,HttpStatus.OK);
-        }
+        try{
+            return new ResponseEntity(userService.update(user), HttpStatus.OK);
 
-        return  ResponseEntity.notFound().build();
+        } catch (NoItemException error){
+            return new ResponseEntity(new ResponseDTO<>(error.getMessage(), user), HttpStatus.BAD_REQUEST);
+        } catch (Exception error){
+            return new ResponseEntity<>(new ResponseDTO<>(error.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable long id){
         try{
-            userRepository.deleteById(id);
+            userService.deleteById(id);
             return new ResponseEntity<>("User with id: " + id + " was deleted", HttpStatus.OK);
         }catch (Exception error){
             return new ResponseEntity<>(error.getMessage(), HttpStatus.NOT_FOUND);
--- src/main/java/com/pdv/pdv/service/UserService.java
+++ src/main/java/com/pdv/pdv/service/UserService.java
@@ -1,19 +1,54 @@
 package com.pdv.pdv.service;
 
 import com.pdv.pdv.dto.UserDTO;
+import com.pdv.pdv.entity.User;
+import com.pdv.pdv.exceptions.NoItemException;
 import com.pdv.pdv.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
-    public List<UserDTO> getAll() {
+    public List<UserDTO> findAll() {
         return userRepository.findAll().stream().map(user ->
                 new UserDTO(user.getId(), user.getName(), user.isEnabled())).collect(Collectors.toList());
     }
+
+    public UserDTO save(User user){
+        userRepository.save(user);
+        return new UserDTO(user.getId(), user.getName(), user.isEnabled());
+    }
+
+    public UserDTO findById(long id){
+        Optional<User> optional = userRepository.findById(id);
+
+        if(!optional.isPresent()){
+            throw new NoItemException("Usuário não encontrado!");
+        }
+        User user = optional.get();
+        return new UserDTO(user.getId(), user.getName(), user.isEnabled());
+
+    }
+
+    public UserDTO update(User user){
+        Optional<User> userToEdit = userRepository.findById(user.getId());
+        if(!userToEdit.isPresent()){
+            throw new NoItemException("Usuário não encontrado!");
+        }
+        userRepository.save(user);
+        return new UserDTO(user.getId(), user.getName(), user.isEnabled());
+    }
+
+    public void deleteById(long id){
+        userRepository.deleteById(id);
+    }
+
 }
