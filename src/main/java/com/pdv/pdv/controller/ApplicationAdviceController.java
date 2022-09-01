f1bfd680f74deee2e2d38aba70740683173bc384
Kaio Antônio <kaioaar@gmail.com>
2022-09-01 11:06:20 -0300

    feat: add generate token

--- src/main/java/com/pdv/pdv/controller/ApplicationAdviceController.java
+++ src/main/java/com/pdv/pdv/controller/ApplicationAdviceController.java
@@ -3,7 +3,9 @@ package com.pdv.pdv.controller;
 import com.pdv.pdv.dto.ResponseDTO;
 import com.pdv.pdv.exceptions.InvalidOperationException;
 import com.pdv.pdv.exceptions.NoItemException;
+import com.pdv.pdv.exceptions.PasswordNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,6 +29,18 @@ public class ApplicationAdviceController {
     public ResponseDTO handleInvalidOperationException(InvalidOperationException ex){
         String messageError = ex.getMessage();
         return new ResponseDTO(messageError);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseDTO handleUsernameNotFoundException(UsernameNotFoundException ex){
+        return new ResponseDTO(ex.getMessage());
+    }
+
+    @ExceptionHandler(PasswordNotFoundException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseDTO handlePasswordNotFoundException(PasswordNotFoundException ex){
+        return new ResponseDTO(ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
--- src/main/java/com/pdv/pdv/controller/LoginController.java
+++ src/main/java/com/pdv/pdv/controller/LoginController.java
@@ -2,8 +2,11 @@ package com.pdv.pdv.controller;
 
 import com.pdv.pdv.dto.LoginDTO;
 import com.pdv.pdv.dto.ResponseDTO;
+import com.pdv.pdv.dto.TokenDTO;
 import com.pdv.pdv.security.CustomUserDetailService;
+import com.pdv.pdv.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,11 +23,18 @@ public class LoginController {
     @Autowired
     private CustomUserDetailService userDetailService;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Value("${security.jwt.expiratrion}")
+    private String expiration;
+
     @PostMapping()
     public ResponseEntity post(@Valid @RequestBody LoginDTO loginData){
         try {
             userDetailService.verifyUserCredentials(loginData);
-            return null;
+            String token = jwtService.generateToken(loginData.getUsername());
+            return new ResponseEntity(new TokenDTO(token,expiration),HttpStatus.OK);
         } catch (Exception error) {
             return new ResponseEntity(new ResponseDTO<>(error.getMessage()), HttpStatus.UNAUTHORIZED);
         }
